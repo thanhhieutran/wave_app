6a0421bc5de598be0c9b9201907d3653a37068cf
HieuTran <tranthanhhieu95@gmail.com>
2023-11-21 08:04:22 +0700

    update layout

--- App/my_package/config.py
+++ App/my_package/config.py
@@ -1,9 +1,9 @@
 # For Database
 data_path = "sqlite3/ProcessDb" # Path to Database (Sqlite3)
 # For check_db.py
+## For checking database
 show_db_info            =   True
 show_kinl_data          =   True
 check_distinct_data     =   True
 # Logo
 bk_logo = 'https://dut.udn.vn/Files/admin/images/Tin_tuc/Khac/2020/LogoDUT/image002.jpg'
-estec_logo = '/home/hieutran/Documents/wave_app/App/media/logo_estec.png'
--- App/my_package/dashboard_general.py
+++ App/my_package/dashboard_general.py
@@ -5,6 +5,7 @@ from .config import *
 
 
 async def show_general_dashboard(q: Q):
+#----- Setup Layout
     q.page['meta'] = ui.meta_card(box='', layouts=[
         ui.layout(
             breakpoint='xs',
@@ -13,130 +14,50 @@ async def show_general_dashboard(q: Q):
                 ui.zone('header', size='76px'),
                 ui.zone('title'),
                 ui.zone('body', size='1000px', zones=[
-                    ui.zone('top', direction=ui.ZoneDirection.ROW, size='25%'),
-                    ui.zone('middle', direction=ui.ZoneDirection.ROW, size='25%'),
-                    ui.zone('middle2', direction=ui.ZoneDirection.ROW, size='25%'),
-                    ui.zone('bottom', direction=ui.ZoneDirection.ROW, size='25%'),
+                    ui.zone('top', direction=ui.ZoneDirection.ROW, size='50%', zones=[
+                        ui.zone('top_left', direction=ui.ZoneDirection.ROW, size='75%'),
+                        ui.zone('top_right'),
+                    ]),
+                    ui.zone('bottom', direction=ui.ZoneDirection.ROW, size='50%'),
                 ]),
-                ui.zone('footer', size='80px'),
+                ui.zone('footer', size='300px'),
             ]
         )
     ])
-
-    q.page['header'] = ui.header_card(box='header', title='H2O Wave Demo', subtitle='Grey Dashboard',
+#----- Header
+    q.page['header'] = ui.header_card(box='header', title='ỨNG DỤNG SỐ HÓA NHÀ MÁY', subtitle='Tình hình hoạt động của nhà máy',
                                       image= bk_logo,
                                       items=[ui.tabs(name='Dashboards', value='#dashboards/general',  
                                                      items=global_nav),])
-    q.page['section'] = ui.section_card(
-        box='title',
-        title=next(sample_title),
-        subtitle=next(sample_caption),
+
+#----- Body
+
+    q.page['layout1'] = ui.form_card(
+        box='top_left',
+        title='top_left',
         items=[
-            ui.label(label='Start:'),
-            ui.date_picker(name='target_date', label='', value='2020-12-20'),
-            ui.label(label='End:'),
-            ui.date_picker(name='target_date', label='', value='2020-12-25'),
+        ],
+    )
+    q.page['layout2'] = ui.form_card(
+        box='middle',
+        title='middle',
+        items=[
+        ],
+    )
+    q.page['layout3'] = ui.form_card(
+        box='bottom',
+        title='bottom',
+        items=[
         ],
     )
 
-    stock_dates = generate_time_series(10000)
-    stock_prices = generate_random_walk()
+ 
+#----- Footer
+    q.page['footer'] = ui.footer_card(box='footer', caption='''
+![estec-logo](https://www.biendongco.vn/resources/img/theme-setting/6-2018/2-logo.png)
 
-    q.page['small'] = ui.small_stat_card(
-        box=ui.box('top', order=1),
-        title=next(sample_term),
-        value=next(sample_dollars),
-    )
-    q.page['small_series'] = ui.small_series_stat_card(
-        box=ui.box('top', order=2),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        plot_category='date',
-        plot_value='price',
-        plot_data=data(
-            fields=['date', 'price'],
-            rows=[(next(stock_dates), next(stock_prices)) for i in range(30)],
-            pack=True,
-        ),
-    )
-    q.page['small_series_interval'] = ui.small_series_stat_card(
-        box=ui.box('top', order=3),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        plot_category='date',
-        plot_value='price',
-        plot_type=ui.SmallSeriesStatCardPlotType.INTERVAL,
-        plot_data=data(
-            fields=['date', 'price'],
-            rows=[(next(stock_dates), next(stock_prices)) for i in range(30)],
-            pack=True,
-        ),
-    )
-    q.page['wide_series'] = ui.wide_series_stat_card(
-        box=ui.box('middle', order=1),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        plot_category='date',
-        plot_value='price',
-        plot_data=data(
-            fields=['date', 'price'],
-            rows=[(next(stock_dates), next(stock_prices)) for i in range(30)],
-            pack=True,
-        ),
-    )
-    q.page['wide_bar'] = ui.wide_bar_stat_card(
-        box=ui.box('middle', order=2),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        progress=random.random(),
-    )
-    q.page['wide_gauge'] = ui.wide_gauge_stat_card(
-        box=ui.box('middle', order=3),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        progress=random.random(),
-    )
-    q.page['tall_series'] = ui.tall_series_stat_card(
-        box=ui.box('middle2', order=1),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        plot_category='date',
-        plot_value='price',
-        plot_data=data(
-            fields=['date', 'price'],
-            rows=[(next(stock_dates), next(stock_prices)) for i in range(30)],
-            pack=True,
-        ),
-    )
-    q.page['tall_gauge'] = ui.tall_gauge_stat_card(
-        box=ui.box('middle2', order=2),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        progress=random.random(),
-    )
-    q.page['large'] = ui.large_stat_card(
-        box=ui.box('bottom', order=1),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        aux_value=next(sample_percent),
-        caption=next(sample_caption),
-    )
-    q.page['large_bar'] = ui.large_bar_stat_card(
-        box=ui.box('bottom', order=2),
-        title=next(sample_term),
-        value=next(sample_dollars),
-        value_caption=next(sample_term),
-        aux_value=next(sample_dollars),
-        aux_value_caption=next(sample_term),
-        progress=random.random(),
-        caption=next(sample_caption),
-    )
-
-    q.page['footer'] = ui.footer_card(box='footer', caption='(c) 2021 H2O.ai. All rights reserved.')
+Sản phẩm phục vụ Đồ Án Tốt Nghiệp được xây dựng bằng H2O Frame - https://wave.h2o.ai/ \n
+Hướng dẫn bởi ESTEC   '''
+)
 
     await q.page.save()
